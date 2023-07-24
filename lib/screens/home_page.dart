bdf017f5c386d3e724bec4ab42b858663b8c2c14
KhaledElalfy1 <khaledalfy53@gmail.com>
2023-07-24 14:21:13 -0700

    commit with bloc

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 import 'package:weather_app_v2/cubit/weather_cubit.dart';
 import 'package:weather_app_v2/cubit/weather_state.dart';
-
 import 'package:weather_app_v2/screens/search_page.dart';
 import 'package:weather_app_v2/screens/statup_page.dart';
 import 'package:weather_app_v2/screens/weather_screen_v2.dart';
